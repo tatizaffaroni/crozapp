4c49e4a329d1c87ac37425dc90e0b57b9651a1ed
tatizaffaroni <tatizaffaroni@yahoo.com.ar>
2024-11-09 15:19:59 -0300

    Nueva funcion que copia la imagen al portapapeles
    Se agrego un boton que copia la imagen al portapapeles.

---
+++ alportapapeles.js
@@ -0,0 +1,19 @@
+document.getElementById("portapapeles").onclick = function () {
+    const screenshotTarget = document.getElementById('fotop');
+
+    html2canvas(screenshotTarget).then((canvas) => {
+        // Convertimos el canvas en una imagen en formato Blob
+        canvas.toBlob((blob) => {
+            // Usamos la API del portapapeles para copiar la imagen al portapapeles
+            navigator.clipboard.write([
+                new ClipboardItem({
+                    "image/png": blob // Especificamos el tipo de la imagen
+                })
+            ]).then(() => {
+                console.log('¡Imagen copiada al portapapeles!');
+            }).catch((error) => {
+                console.error('Error al copiar al portapapeles: ', error);
+            });
+        }, 'image/png');
+    });
+};
